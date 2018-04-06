3bc468470237826a617f71b36bd0d4b49132f2ed
YancyZheng <zhengyunda@outlook.com>
2018-04-06 23:25:26 +0800

    add sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
+import './App.css';
+
 const Home = () => (
     <div>
         <h2>Home</h2>
